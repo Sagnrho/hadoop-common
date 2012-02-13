53a8d757d2d04f00e0ccee58f3a734c4106aabf0
Constantine Peresypkin <constantine@celltick.com>
2012-02-13 15:05:01 +0200

    licensing, class headers

--- src/core/org/apache/hadoop/fs/swift/SwiftFileSystem.java
+++ src/core/org/apache/hadoop/fs/swift/SwiftFileSystem.java
@@ -1,3 +1,21 @@
+/**
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package org.apache.hadoop.fs.swift;
 
 import java.io.FileNotFoundException;
@@ -43,6 +61,15 @@ import com.rackspacecloud.client.cloudfiles.FilesNotFoundException;
 import com.rackspacecloud.client.cloudfiles.FilesObject;
 import com.rackspacecloud.client.cloudfiles.FilesObjectMetaData;
 
+/**
+ * @author Constantine Peresypkin
+ *
+ * <p>
+ * A distributed implementation of {@link FileSystem} 
+ * for reading and writing files on 
+ * <a href="http://swift.openstack.org/">Openstack Swift</a>.
+ * </p>
+ */
 public class SwiftFileSystem extends FileSystem {
 
 	private static final long MAX_SWIFT_FILE_SIZE = 5 * 1024 * 1024 * 1024L;
